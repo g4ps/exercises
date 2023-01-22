a70018dfc0b906939c0938768d0a68c0805a2275
g4ps <eum4rckin@yandex.ru>
2023-01-23 00:05:49 +0300

    Finished for today

--- cormen_algorithms/progs/ch02/sort.c
+++ cormen_algorithms/progs/ch02/sort.c
@@ -22,7 +22,7 @@ void print_arr(int *arr, int size) {
 }
 
 void sort(int *arr, int size) {
-  for (int i = i; i < size; i++) {
+  for (int i = 1; i < size; i++) {
     int key = arr[i];
     /* int j = i - i; */
     int j;
@@ -33,10 +33,26 @@ void sort(int *arr, int size) {
   }
 }
 
+void rev_sort(int *arr, int size) {
+  for (int i = 1; i < size; i++) {
+    int key = arr[i];
+    /* int j = i - i; */
+    int j;
+    for (j = i - 1; j >= 0 && arr[j] < key; j--) {
+      arr[j + 1] = arr[j];      
+    }
+    arr[j + 1] = key;
+  }
+}
+
 int main() {
   srand(time(NULL));
   int size = (rand() % 10) + 10;  
-  int arr* = calloc(0, size + 1);
+  int *arr = calloc(size + 1, sizeof(int));
   fill_rand_arr(arr, size);
+  print_arr(arr, size);
+  /* Pick either one of the latter two lines to get normal and reverse sort */
+  sort(arr, size);
+  /* rev_sort(arr, size); */
   print_arr(arr, size);
 }
